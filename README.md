b7fd807204e9142c40bf23a449e822199cc9e2a8
raurir <raurir@gmail.com>
2019-05-06 11:45:26 +1000

    followers etc

--- README.md
+++ README.md
@@ -1,8 +1,49 @@
 # Twitter Social Bot
 
-Basic handy Twitter methods.
+Basic wrapper around twitter module with handy methods.
 
-### Used by:
+## Usage
+
+### Install dependencies
+
+```bash
+yarn
+```
+
+### Get twitter credentials
+
+[Google it... :)](https://www.google.com/search?q=twitter+api+credentials)
+
+### Create entry point
+
+something like `index.js`, and add this:
+
+```javascript
+socialbot.initClient({
+	consumer_key: "get",
+	consumer_secret: "your",
+	access_token_key: "own",
+	access_token_secret: "credentials"
+});
+
+socialbot
+	.getFollowers()
+	.then(followers => {
+		console.log("socialbot got followers:", followers.ids.length);
+	})
+	.catch(err => {
+		console.log("something went wrong", err);
+	});
+```
+
+### Run it
+
+```bash
+node index.js
+```
+
+## Used by:
 
 - [@AnsiBot](http://www.twitter.com/AnsiBot) / [github.com/raurir/AnsiBot](https://github.com/raurir/AnsiBot)
 - [@StatsOfOrigin](http://www.twitter.com/StatsOfOrigin) / [github.com/raurir/StatsOfOrigin](https://github.com/raurir/StatsOfOrigin)
+- [@FunkyVector](http://www.twitter.com/FunkyVector) / Infinite print project
--- socialbot.js
+++ socialbot.js
@@ -1,26 +1,22 @@
-var Promise = require('promise');
+var Promise = require("promise");
 var Twitter = require("twitter");
 
-var con = console;
-
 module.exports = (function() {
-
   var client;
   var hits = 0; // local testing limit.
 
   function initClient(authsettings) {
     if (client) return client;
-    // con.log("initialising client");
+    // console.log("initialising client");
     client = new Twitter(authsettings);
-    // con.log("client", client)
+    // console.log("client", client)
     return client;
   }
 
-
   function getUserFromId(user_id) {
-    con.log("Socialbot getUserFromId", user_id);
+    console.log("Socialbot getUserFromId", user_id);
     return new Promise(function(fulfill, reject) {
-      client.get('users/show', {user_id}, function(error, reply) {
+      client.get("users/show", { user_id }, function(error, reply) {
         if (error) {
           reject(error);
         } else {
@@ -31,9 +27,9 @@ module.exports = (function() {
   }
 
   function getUserFromName(screen_name) {
-    con.log("Socialbot getUserFromName", screen_name);
+    console.log("Socialbot getUserFromName", screen_name);
     return new Promise(function(fulfill, reject) {
-      client.get('users/show', {screen_name}, function(error, reply) {
+      client.get("users/show", { screen_name }, function(error, reply) {
         if (error) {
           reject(error);
         } else {
@@ -44,14 +40,14 @@ module.exports = (function() {
   }
 
   function getFollowers() {
-    // con.log("getFollowers");
+    // console.log("getFollowers");
     return new Promise(function(fulfill, reject) {
-      client.get('followers/ids', function(error, reply) {
+      client.get("followers/ids", function(error, reply) {
         if (error) {
-          // con.log("getFollowers error:", error);
+          // console.log("getFollowers error:", error);
           reject(error);
         } else {
-          // con.log("getFollowers success:", reply);
+          // console.log("getFollowers success:", reply);
           fulfill(reply);
           // var followers = reply.ids; //, randFollower = randIndex(followers);
         }
@@ -59,185 +55,247 @@ module.exports = (function() {
     });
   }
 
-
-
   function getFollowing(user_id) {
-    con.log("getFollowing", user_id);
+    console.log("getFollowing", user_id);
     return new Promise(function(fulfill, reject) {
       try {
-        con.log("getFollowing inside promise", user_id);
+        console.log("getFollowing inside promise", user_id);
         var param = user_id ? { user_id: user_id } : {};
-        client.get('friends/ids', param, function(error, reply) {
+        client.get("friends/ids", param, function(error, reply) {
           if (error) {
-            con.log("get friend ids", error);
+            console.log("get friend ids", error);
             reject(error);
           } else {
             var friends = reply.ids;
             if (friends.length) {
-              con.log("getFollowing of", user_id, friends.length);//, friends.join(" / "));
+              console.log("getFollowing of", user_id, friends.length); //, friends.join(" / "));
               fulfill(friends);
             } else {
-              con.log("rejected no friends...")
+              console.log("rejected no friends...");
               reject("NO_FRIENDS");
             }
           }
-        })
-      } catch(err) {
-        con.log("getFollowing err", err);
+        });
+      } catch (err) {
+        console.log("getFollowing err", err);
         reject();
       }
-    })
+    });
+  }
+
+  function getTweetRate(screen_names) {
+    return new Promise(function(fulfill, reject) {
+      try {
+        if (!Array.isArray(screen_names))
+          return reject(
+            "getTweetRate expects array of screen_names!",
+            screen_names
+          );
+
+        screen_names.forEach(screen_name => {
+          getTweets({ screen_name, count: 600 }).then(tweets => {
+            const months = [];
+            const tweetsPerMonth = [];
+            tweets.forEach(tweet => {
+              const t = tweet.created_at;
+              const d = new Date(t);
+              const y = d.getFullYear();
+              let m = d.getMonth() + 1;
+              if (String(m).length === 1) m = "0" + m;
+
+              let ym = `${y}-${m}`;
+              let index = months.indexOf(ym);
+
+              if (index === -1) {
+                months.push(ym);
+                index = months.length - 1;
+                tweetsPerMonth[index] = 0;
+              }
+
+              tweetsPerMonth[index]++;
+            });
+
+            console.log(`Tweet rate for ${screen_name}`);
+            tweetsPerMonth.forEach((tpm, index) => {
+              let ym = months[index];
+              console.log(
+                `for month ${ym} tweets per day: ${(tpm / 30).toFixed(
+                  1
+                )} and per month: ${tpm}`
+              );
+            });
+            console.log(`------------------`);
+            fulfill(tweetsPerMonth);
+          });
+        });
+      } catch (err) {
+        console.log("getTweetRate err", err);
+        reject();
+      }
+    });
   }
 
   function getTweets(params) {
-    con.log("SocialBot.getTweets attempt", params);
+    console.log("SocialBot.getTweets attempt", params);
     return new Promise(function(fulfill, reject) {
       if (params.id || params.screen_name) {
         try {
-          client.get('statuses/user_timeline', params, function(error, response) {
+          client.get("statuses/user_timeline", params, function(
+            error,
+            response
+          ) {
             if (error) {
-              con.log("getTweets error 01", error);
+              console.log("getTweets error 01", error);
               reject(error);
             } else {
-              // con.log("=====================================");
-              // con.log("followFriend fulfill response", response);
-              // con.log("=====================================");
-              // con.log("followFriend fulfill name:", response.name, "location:", response.location, "description:", response.description, "url:", response.url);
+              // console.log("=====================================");
+              // console.log("followFriend fulfill response", response);
+              // console.log("=====================================");
+              // console.log("followFriend fulfill name:", response.name, "location:", response.location, "description:", response.description, "url:", response.url);
               fulfill(response);
             }
           });
-        } catch(err) {
-          con.log("SocialBot.getTweets error 02", err);
+        } catch (err) {
+          console.log("SocialBot.getTweets error 02", err);
           reject(err);
         }
       } else {
-        con.log("SocialBot.getTweets error 03 no user id!");
+        console.log("SocialBot.getTweets error 03 no user id!");
         reject(null);
       }
     });
   }
 
-
   function followFriend(user_id) {
-    // con.log("followFriend attempt", user_id);
+    // console.log("followFriend attempt", user_id);
     return new Promise(function(fulfill, reject) {
       if (user_id) {
         try {
-          client.post('friendships/create', {id: user_id}, function(error, response) {
+          client.post("friendships/create", { id: user_id }, function(
+            error,
+            response
+          ) {
             if (error) {
-              con.log("followFriend error 01", error);
+              console.log("followFriend error 01", error);
               reject(error);
             } else {
-              // con.log("=====================================");
-              // con.log("followFriend fulfill response", response);
-              // con.log("=====================================");
-              // con.log("followFriend fulfill name:", response.name, "location:", response.location, "description:", response.description, "url:", response.url);
+              // console.log("=====================================");
+              // console.log("followFriend fulfill response", response);
+              // console.log("=====================================");
+              // console.log("followFriend fulfill name:", response.name, "location:", response.location, "description:", response.description, "url:", response.url);
               fulfill(response);
             }
           });
-        } catch(e) {
-          con.log("followFriend error 02", e);
+        } catch (e) {
+          console.log("followFriend error 02", e);
           reject(e);
         }
       } else {
-        con.log("followFriend no friend to follow!");
+        console.log("followFriend no friend to follow!");
         reject(null);
       }
     });
   }
 
-
   function unfollowFriend(user_id) {
-    // con.log("unfollowFriend", user_id);
+    // console.log("unfollowFriend", user_id);
     return new Promise(function(fulfill, reject) {
       if (user_id) {
         try {
-          client.post('friendships/destroy', {id: user_id}, function(error, response) {
+          client.post("friendships/destroy", { id: user_id }, function(
+            error,
+            response
+          ) {
             if (error) {
-              con.log("unfollowFriend error 01", error);
+              console.log("unfollowFriend error 01", error);
               reject(error);
             } else {
-              con.log("unfollowFriend success");
-              // con.log("unfollowFriend fulfill response", response);
+              console.log("unfollowFriend success");
+              // console.log("unfollowFriend fulfill response", response);
               fulfill(user_id);
             }
           });
-        } catch(e) {
-          con.log("unfollowFriend error 02", e);
+        } catch (e) {
+          console.log("unfollowFriend error 02", e);
           reject(e);
         }
       } else {
-        con.log("unfollowFriend no friend to follow!");
+        console.log("unfollowFriend no friend to follow!");
         reject(null);
       }
     });
   }
 
-
-
   function postMedia(image) {
     return new Promise(function(fulfill, reject) {
+      console.log("postMedia", image);
 
-      con.log("postMedia", image);
-
-      hits ++;
+      hits++;
       if (hits > 2) {
-        // con.log("too many hits!");
+        // console.log("too many hits!");
         reject(new Error("more than 5 hits!"));
       }
 
-      con.log("trying client.post!", client);
+      console.log("trying client.post!", client);
       try {
-
-        client.post("media/upload", {media: image}, function(error, media, response){
+        client.post("media/upload", { media: image }, function(
+          error,
+          media,
+          response
+        ) {
           if (error) {
-            con.log("postMedia reject 01", error);
+            console.log("postMedia reject 01", error);
             reject(error);
           } else {
-            // con.log("postMedia fulfill!");
+            // console.log("postMedia fulfill!");
             fulfill(media);
           }
         });
-
       } catch (e) {
-        con.log("postMedia reject 03", e);
+        console.log("postMedia reject 03", e);
         reject(e);
       }
-
     });
   }
 
   function postTweet(status) {
     return new Promise(function(fulfill, reject) {
-      // con.log("postTweet media success", status);
+      // console.log("postTweet media success", status);
       try {
-        client.post("statuses/update", status, function(error, tweet, response){
+        client.post("statuses/update", status, function(
+          error,
+          tweet,
+          response
+        ) {
           if (error) {
-            con.log("postTweet reject 02", error);
+            console.log("postTweet reject 02", error);
             reject(error);
           } else {
-            // con.log("postTweet fulfill");
+            // console.log("postTweet fulfill");
             fulfill(tweet);
           }
         });
       } catch (e) {
-        con.log("postTweet reject 01", e);
+        console.log("postTweet reject 01", e);
         reject(e);
       }
-    })
+    });
   }
 
   return {
-    followFriend: followFriend,
-    getFollowers: getFollowers,
-    getFollowing: getFollowing, // used to be getFriends!
-    getFriends: function() { con.warn('getFriends deprecated! use getFollowing')},
-    getTweets: getTweets,
+    followFriend,
+    getFollowers,
+    getFollowing, // used to be getFriends!
+    getFriends: function() {
+      console.warn("getFriends deprecated! use getFollowing");
+    },
+    getTweetRate,
+    getTweets,
     getUserFromId,
     getUserFromName,
-    initClient: initClient,
-    postMedia: postMedia,
-    postTweet: postTweet,
-    unfollowFriend: unfollowFriend,
-  }
+    initClient,
+    postMedia,
+    postTweet,
+    unfollowFriend
+  };
 })();
